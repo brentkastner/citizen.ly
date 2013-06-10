eb5030a672302ce3d6ff67e814387fa3dd30cbda
Brent Kastner <brentkastner@gmail.com>
2013-06-10 12:21:58 -0500

    Scaffolded Questions

---
+++ app/models/question.rb
@@ -0,0 +1,3 @@
+class Question < ActiveRecord::Base
+  attr_accessible :detail, :question
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Polis::Application.routes.draw do
+  resources :questions
+
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Polis::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
