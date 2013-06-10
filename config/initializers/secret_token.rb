17f97fa1512dae98052e7328558147943478ba2f
Brent Kastner <brentkastner@gmail.com>
2013-06-10 12:00:03 -0500

    Initial Polis Project Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polis::Application.config.secret_token = '22779c47671d2cadf1ed687b8090f1a7dec4d2abb9d8b882e0ea353b5fd1814aec84cd83a8f081070ae23636cc810eed43490a78fad657f7883f7d4e1778799d'
